de053c6fafe11db772a27b2b29450c7e7b303a12
adorableco <tpdusdl1001@knu.ac.kr>
2023-09-28 19:37:26 +0900

    chatGPT 대화페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route, Router } from "react-router-dom";
 import Home from "./components/pages/main/Home";
 import Detail from "./components/pages/detail/Detail";
 import BoardWrite from "./components/pages/boards/BoardWrite";
 import BoardUpdate from "./components/pages/boards/BoardUpdate";
+import Chat from "./components/Chat";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/detail/:idx" element={<Detail />} />
         <Route path="/" element={<Home />} />
-        <Route path="/write" element={<BoardWrite />} /> 
+        <Route path="/write" element={<BoardWrite />} />
         <Route path="*" element={<Home />} />
         <Route path="/update/:idx" element={<BoardUpdate />} />
+        <Route path="/chatgpt" element={<Chat />} />
       </Routes>
     </div>
   );
