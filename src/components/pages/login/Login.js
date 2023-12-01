a9167230f8daa88e439e5dd97a319cd8f48110c8
adorableco <tpdusdl1001@knu.ac.kr>
2023-12-01 17:49:29 +0900

    ✨ Feat: "구글 로그인 연결 및 사용자 정보 표시 (프로필 페이지 내)"
    resolves : # 25

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,26 +1,34 @@
 /** @format */
 // Login과 Signup 페이지는 UI가 거의 동일합니다.
-import React from "react";
+import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { useLocation, useNavigate } from "react-router-dom";
 import Navigation from "../../Navigation";
 import "./Login.css";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 const Login = () => {
   const google = async () => {
     try {
-      await (axios.get("/auth/kakao",{
-        headers : {"Access-Code" : "L8ZHluIb_u5XKuEHa3GrJQMBxeJuWZ5cnRUb5TgYtXFcnLkcdYoGsuI1AXwKKiWRAAABi-alyAbOkqTnJF629A"}
-      }))
+      await axios.get("/auth/kakao", {
+        headers: {
+          "Access-Code":
+            "L8ZHluIb_u5XKuEHa3GrJQMBxeJuWZ5cnRUb5TgYtXFcnLkcdYoGsuI1AXwKKiWRAAABi-alyAbOkqTnJF629A",
+        },
+      });
+    } catch (error) {
+      console.log(error);
     }
-  catch(error) {
-    console.log(error);
-  }};
+  };
+
+  const login = () => {
+    window.location.href = "https://a-hi-prompt.com/google-login";
+  };
   const naver = () => {
-    axios.get("/auth/naver",{
-      headers : {"Access-Code" : "O1HrSzrucATjL7qh05"}
-    })
+    axios.get("/auth/naver", {
+      headers: { "Access-Code": "O1HrSzrucATjL7qh05" },
+    });
   };
   return (
     <div className='Login'>
@@ -36,7 +44,7 @@ const Login = () => {
       >
         <h2>안녕 AI</h2>
         <h1>에이 하이</h1>
-        <Link to='/signup' style={{ color: "inherit"}}>
+        <Link to='/signup' style={{ color: "inherit" }}>
           회원이 아니신가요?
         </Link>
         <p style={{ marginTop: "30px" }}>이메일로 로그인하기</p>
@@ -49,10 +57,20 @@ const Login = () => {
           </div>
           <button type='submit'>로그인</button>
 
-          <div style={{marginTop:"30px",marginLeft:"50px"}}>
-              <img style={{display:"block", width:"300px",marginBottom:"8px"}} src="img/google_login.png" onClick={()=>{google()}}/>
-              <img style={{display:"block", width:"300px", height:"75px"}} src="img/naver_login.png" onClick={()=>{naver()}}/>
-            </div>
+          <div style={{ marginTop: "30px", marginLeft: "50px" }}>
+            <img
+              style={{ display: "block", width: "300px", marginBottom: "8px" }}
+              src='img/google_login.png'
+              onClick={login}
+            />
+            <img
+              style={{ display: "block", width: "300px", height: "75px" }}
+              src='img/naver_login.png'
+              onClick={() => {
+                naver();
+              }}
+            />
+          </div>
         </form>
       </div>
     </div>
--- src/components/pages/main/Home.jsx
+++ src/components/pages/main/Home.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect, useState} from 'react';
-import {Link, useNavigate} from "react-router-dom";
+import {Link, useNavigate,useLocation} from "react-router-dom";
 import axios from "axios";
 import {BASE_URL} from "../../../assets/Strings";
 import Navigation from "../../Navigation";
@@ -9,7 +9,9 @@ import { BiSolidConversation } from "react-icons/bi";
 import "./Home.css";
 
 const Home = () => {
-    const navigate=useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
+
     const [category,setCategory] = useState(1);
     const [boardList, setBoardList] = useState([]);
     const [mainKind,setMainKind]=useState(1)
@@ -42,7 +44,36 @@ const Home = () => {
   getList()
   }
   ,[])
+
+
+    useEffect(() => {
+      // 현재 페이지의 쿼리 스트링을 가져옴
+      const queryString = window.location.search;
   
+      // 쿼리 스트링이 비어있지 않으면 (기본 주소 뒤에 쿼리가 있다면)
+      if (queryString !== '') {
+        // 실행할 작업 수행
+        console.log('기본 주소 뒤에 쿼리가 붙어 있습니다.');
+
+        console.log("쿼리문이 있는 페이지입니다.");
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const memberId = urlParams.get("member_id");
+        const nickname = urlParams.get("nickname");
+        const profileImage = urlParams.get("profile_image");
+        const jwtToken = urlParams.get("jwt");
+  
+        // 추출한 값들을 localStorage에 저장
+        localStorage.setItem("memberId", memberId);
+        localStorage.setItem("nickname", nickname);
+        localStorage.setItem("profileImage", profileImage);
+        localStorage.setItem("jwtToken", jwtToken);
+  
+        // 예시: 다른 페이지로 리다이렉트
+        // window.location.href = '/new-page';
+      }
+    }, []);
+
 
 
   return (
